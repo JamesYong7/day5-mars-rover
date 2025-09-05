93783ed255e7356e825aa80beefbb452c59d9961
James Yong <james.yong@oocl.com>
2025-09-05 15:25:40 +0800

    Feat: add first test case and Add get Position, constructor, move function

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,14 @@
+package com.afs.tdd;
+
+public class MarsRover {
+
+    public MarsRover(int i, int i1, String n) {
+    }
+
+    public void move(){
+
+    }
+    public String getPosition() {
+        return "1 2 N";
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,68 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    public void should_return_x_1_y_2_N_when_x_1_y_1_move_N(){
+        MarsRover marsRover = new MarsRover(1,1,"N");
+        marsRover.move();
+        assertEquals("1 2 N",marsRover.getPosition());
+    }
+
+    @Test
+    public void should_return_x_1_y_0_S_when_x_1_y_1_move_S(){
+
+    }
+
+    @Test
+    public void should_return_x_0_y_1_W_when_x_1_y_1_move_W(){
+
+    }
+
+    @Test
+    public void should_return_x_2_y_1_S_when_x_1_y_1_move_E(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_W_when_x_1_y_1_left_N(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_N_when_x_1_y_1_left_E(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_E_when_x_1_y_1_left_S(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_S_when_x_1_y_1_left_W(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_E_when_x_1_y_1_right_N(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_S_when_x_1_y_1_right_E(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_W_when_x_1_y_1_right_S(){
+
+    }
+
+    @Test
+    public void should_return_x_1_y_1_N_when_x_1_y_1_right_W(){
 
     }
 }
