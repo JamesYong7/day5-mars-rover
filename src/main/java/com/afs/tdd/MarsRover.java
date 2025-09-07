455528b982692745decf822d0a637bd854cec3d8
James Yong <james.yong@oocl.com>
2025-09-07 13:37:58 +0800

    Refactor: use hashmap to store the direction to avoid multiple layer if-else

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,9 +1,23 @@
 package com.afs.tdd;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MarsRover {
     private static final String[] DIRECTIONS = {"N", "E", "S", "W"};
+    private static final Map<String, int[]> MOVE_MAP = new HashMap<>();
+    private static final Map<String, int[]> BACK_MAP = new HashMap<>();
+    static {
+        MOVE_MAP.put("N", new int[]{0, 1});
+        MOVE_MAP.put("E", new int[]{1, 0});
+        MOVE_MAP.put("S", new int[]{0, -1});
+        MOVE_MAP.put("W", new int[]{-1, 0});
+        BACK_MAP.put("N", new int[]{0, -1});
+        BACK_MAP.put("E", new int[]{-1, 0});
+        BACK_MAP.put("S", new int[]{0, 1});
+        BACK_MAP.put("W", new int[]{1, 0});
+    }
 
     private int x;
     private int y;
@@ -16,48 +30,20 @@ public class MarsRover {
     }
 
     public void execute(Command command) {
-        switch (command) {
-            case M:
-                switch (direction) {
-                    case "N":
-                        this.y++;
-                        break;
-                    case "S":
-                        this.y--;
-                        break;
-                    case "W":
-                        this.x--;
-                        break;
-                    case "E":
-                        this.x++;
-                        break;
-                }
-                break;
-            case L:
-                int leftIndex = Arrays.asList(DIRECTIONS).indexOf(this.direction);
-                this.direction = DIRECTIONS[(leftIndex + 3) % DIRECTIONS.length];
-                break;
-            case R:
-                int rightIndex = Arrays.asList(DIRECTIONS).indexOf(this.direction);
-                this.direction = DIRECTIONS[(rightIndex + 1) % DIRECTIONS.length];
-                break;
-            case B:
-                switch (direction) {
-                    case "N":
-                        this.y--;
-                        break;
-                    case "S":
-                        this.y++;
-                        break;
-                    case "W":
-                        this.x++;
-                        break;
-                    case "E":
-                        this.x--;
-                        break;
-                }
-                break;
-
+        if (command == Command.M) {
+            int[] move = MOVE_MAP.get(direction);
+            this.x += move[0];
+            this.y += move[1];
+        } else if (command == Command.B) {
+            int[] move = BACK_MAP.get(direction);
+            this.x += move[0];
+            this.y += move[1];
+        } else if (command == Command.L) {
+            int leftIndex = Arrays.asList(DIRECTIONS).indexOf(this.direction);
+            this.direction = DIRECTIONS[(leftIndex + 3) % DIRECTIONS.length];
+        } else if (command == Command.R) {
+            int rightIndex = Arrays.asList(DIRECTIONS).indexOf(this.direction);
+            this.direction = DIRECTIONS[(rightIndex + 1) % DIRECTIONS.length];
         }
     }
 
